1829965604f46265d7ce29a7f6eb1846a9ed13a8
Doelzizz <48619448+AbdulAziz713@users.noreply.github.com>
2024-09-23 14:13:52 +0700

    Delete Latihan1.2.php

--- Latihan1.2.php
+++
@@ -1,8 +0,0 @@
-class Mobil{
-//ini adalah atribut/properties dari mobil
-    <var> $jumlahRoda = 4</var>
-    <var> $warna = "Merah"</var>
-    <var> $bahanBakar = "Pertamax"</var>
-    <var> $harga = 120000000</var>
-    <var> $merk = 'A'</var>
-}
