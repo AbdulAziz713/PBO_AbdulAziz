78b5364cdcdb521cc56dfbb696319c8b739210c9
Doelzizz <48619448+AbdulAziz713@users.noreply.github.com>
2024-09-24 00:14:57 +0700

    Update Latihan7.3.php

--- Latihan7.3.php
+++ Latihan7.3.php
@@ -1,17 +1,21 @@
 <?php
-class Shape {
-const PI = 3.142 ;
-    function __call($name,$arg){
-    if($name == 'area')
-        switch(count($arg)){
-        case 0 : return 0 ;
-        case 1 : return self::PI * $arg[0] ;
-        case 2 : return $arg[0] * $arg[1];
-        }
+class Base {
+    function display() {
+    echo "\nBase class function declared final!";
+    }
+    function demo() {
+    echo "\nBase class function!";
     }
 }
-$circle = new Shape();
-echo $circle->area(3);
-$rect = new Shape();
-echo $rect->area(8,6);
+class Derived extends Base {
+    function demo() {
+    echo "\nDerived class function!";
+    }
+}
+$ob = new Base;
+$ob->demo();
+$ob->display();
+$ob2 = new Derived;
+$ob2->demo();
+$ob2->display();
 ?>
